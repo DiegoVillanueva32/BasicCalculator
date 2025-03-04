19606b7009c26f23685d816ed3c0578f0f64de72
Sala315 <d.villanueva0504@gmail.com>
2025-03-03 20:17:50 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator MyCalculator = new Calculator();
+        System.out.println(MyCalculator.add(5, 7));
+        System.out.println(MyCalculator.subtract(45, 11));
+        System.out.println(MyCalculator.multiply(4, 8));
+        System.out.println(MyCalculator.divide(58, 21));
+        System.out.println(MyCalculator.divide(154, 3));
+    }
+}
